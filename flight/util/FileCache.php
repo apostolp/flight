39b453fcecde2ba58775805758a45b3b6a31a0bc
apostolp <pasha.pashenko@gmail.com>
2013-01-11 17:41:16 +0200

    - added util/FileCache.php

---
+++ flight/util/FileCache.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace flight\util;
+
+/**
+ * FileCache - store object on file system
+ *
+ * Examples
+ * Cache::setCacheDir('Cache');
+ * Cache::setObject('obj', $obj);
+ * Cache::getObject('obj');
+ *
+ */
+class FileCache
+{
+
+    private static $dir = '';
+
+    public static $error = '';
+
+    private static function filename ($str)
+    {
+        return base64_encode ($str);
+    }
+
+    /**
+     * @param $id_str_name
+     * @param $object
+     * @param $time_in_sec - defaults 180
+     * @return boolean
+     */
+    public static function setObject ($id_str_name, $object, $time_in_sec = 180)
+    {
+        return (file_put_contents (self::$dir . '/' . self::filename($id_str_name), $time_in_sec . PHP_EOL . serialize($object))!== false);
+    }
+
+    /**
+     * @param $id_str_name
+     * @return object or false if object has been expired or does not exists
+     */
+    public static function getObject ($id_str_name)
+    {
+        $path = self::$dir . '/' . self::filename($id_str_name);
+        $str = file ($path);
+
+        if ($str === false) return false;
+
+        if ((time() - filemtime($path)) > intval($str[0])) return false;
+
+        return unserialize($str[1]);
+    }
+
+    /**
+     * @param $path
+     * @return bool
+     */
+    public static function setCacheDir($path)
+    {
+        self::$dir = $path;
+
+        if (!file_exists(self::$dir)) return mkdir(self::$dir);
+
+        return is_dir(self::$dir);
+    }
+
+    public static function clear()
+    {
+        $it = new RecursiveDirectoryIterator(self::$dir);
+        $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+
+        foreach($files as $file) {
+
+            if ($file->isDir()) {
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+
+    }
+}
